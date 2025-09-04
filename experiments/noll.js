f2447ce6a9bd54b32107132972426ec8fd067d14
vani23du <vani23du@student.ju.se>
2025-09-04 08:20:38 +0200

    More small changes to the Monar variation one

--- experiments/noll.js
+++ experiments/noll.js
@@ -1,18 +1,1 @@
-function setup() {
-  createCanvas(innerWidth, innerHeight);
-  background(255, 255, 255);
-  x = random(width);
-  y = random(height);
-}
 
-function randomLines() {
-  for (let i = 0; i < 20; i++);
-}
-
-function draw() {
-  background(255, 255, 255);
-  stroke(0, 0, 0);
-  randomLines();
-}
-
-noLoop();
